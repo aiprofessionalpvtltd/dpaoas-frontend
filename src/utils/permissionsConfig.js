d0633fc009ba8859ed920df0fa8d0d2c12143832
itsmohsinali <itsmohsinali5@gmail.com>
2023-12-11 12:36:17 +0500

    Permissions

--- src/utils/permissionsConfig.js
+++ src/utils/permissionsConfig.js
@@ -1,9 +1,9 @@
 export const CheckPermission = (role, roles, permissions) => {
-  const rolesDummy = ["HR", "Admin", "SuperAdmin"];
+  // const rolesDummy = ["HR", "Admin", "SuperAdmin"];
   const modulePermissions = {};
 
   permissions.forEach((module) => {
-    if (rolesDummy.includes(role)) {
+    if (roles.includes(role)) {
       modulePermissions[module.label] = {
         canView: module.hasAccess.includes("View"),
         canEdit: module.hasAccess.includes("Edit"),
