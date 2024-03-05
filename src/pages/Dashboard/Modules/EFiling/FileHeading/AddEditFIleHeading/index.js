1fc0c10cbeca7e651179cabc4ee6902aa690d2e6
Muhammad Saqib Khan <muhammadsaqibkhan01@gmail.com>
2024-03-05 10:34:57 +0500

    heading change in efiling

--- src/pages/Dashboard/Modules/EFiling/FileHeading/AddEditFIleHeading/index.js
+++ src/pages/Dashboard/Modules/EFiling/FileHeading/AddEditFIleHeading/index.js
@@ -113,7 +113,7 @@ function AddEditFIleHeading() {
 
   return (
     <Layout sidebarItems={EfilingSideBarItem} module={true}>
-    <Header dashboardLink={"/efiling/dashboard/file-heading-list"} addLink1={"/efiling/dashboard/addedit-file-register"} title1={"Add File Register"} width={"500px"} />
+    <Header dashboardLink={"/efiling/dashboard/file-heading-list"} addLink1={"/efiling/dashboard/addedit-file-register"} title1={location?.state ? "Edit File Heading" : "Add File Heading"} width={"500px"} />
     <ToastContainer />
     <div class="container-fluid">
       <div class="card">
