a48182e181c5831a417f4fd5eacf61d67cc958df
MuhammadSaqibKhan <muhammadsaqibkhan01@gmail.com>
2024-03-06 16:50:13 +0500

    research services and perferma for prevision api in notice office

--- src/pages/Dashboard/Modules/Notice/ResearchServices/AddEditResearchService/index.js
+++ src/pages/Dashboard/Modules/Notice/ResearchServices/AddEditResearchService/index.js
@@ -14,73 +14,145 @@ import { Layout } from "../../../../../../components/Layout";
 import { NoticeSidebarItems } from "../../../../../../utils/sideBarItems";
 import { AuthContext } from "../../../../../../api/AuthContext";
 import Header from "../../../../../../components/Header";
+import {
+  UpdateResearchServices,
+  createResearchServices,
+  getResearchServicesById,
+} from "../../../../../../api/APIs/Services/Notice.service";
+import {
+  showErrorMessage,
+  showSuccessMessage,
+} from "../../../../../../utils/ToastAlert";
 
 function CMSAddEditResearchService() {
   const location = useLocation();
-  const { employeeData } = useContext(AuthContext);
+  const [researchData, setresearchData] = useState([]);
+  const serviceTypeData = [
+    {
+      id: 1,
+      description: "Statistical Data",
+    },
+    {
+      id: 2,
+      description: "Brief",
+    },
+    {
+      id: 3,
+      description: "General Information",
+    },
+    {
+      id: 4,
+      description: "Talking Points",
+    },
+    {
+      id: 5,
+      description: "Backgrounders",
+    },
+    {
+      id: 6,
+      description: "Country Papers/Profiles",
+    },
+    {
+      id: 7,
+      description: "Statistical Analyses",
+    },
+    {
+      id: 8,
+      description: "Research Reports from Other Sources",
+    },
+    {
+      id: 9,
+      description: "Basic Information",
+    },
+  ];
 
   // Validation Schema For Toner Installation
   const validationSchema = Yup.object({
-    sessionNo: Yup.string().required("Session No is required"),
-    fromDate: Yup.object().required("From Date is required"),
-    toDate: Yup.string().required("To Date is required"),
-    justification: Yup.object().required("Justification is required"),
+    service_type: Yup.object().required("Service Type is required"),
+    details: Yup.string().required("Details is required"),
   });
   const formik = useFormik({
     initialValues: {
-      sessionNo: location.state ? new Date(location?.state?.requestDate) : "",
-      fromDate: location.state ? location?.state?.fkBranchRequestId : "",
-      toDate: location.state ? location?.state?.fkBranchRequestId : "",
-
-      justification: location.state ? location?.state?.quantity : "",
-      status: location.state ? location?.state?.status : "",
+      service_type: "",
+      details: "",
     },
-    // validationSchema: validationSchema,
+    validationSchema: validationSchema,
     onSubmit: (values) => {
       // Handle form submission here
-      console.log(values);
+      if (location?.state?.id) {
+        UpdateResearchServicesApi(values);
+      } else {
+        hendleCreateResarchServices(values);
+      }
     },
   });
 
-  // Handle Create Toner Installation
-  //   const hendleAddToner = async (values) => {
-  //     const Data = {
-  //       requestDate: values?.requestDate,
-  //       fkUserRequestId: values?.fkUserRequestId?.value,
-  //       fkBranchRequestId: values?.fkBranchRequestId,
-  //       fkTonerModelId: values?.tonerModels?.value,
-  //       quantity: values?.quantity,
-  //     };
+  // Handle Create Reseacrch Services
+  const hendleCreateResarchServices = async (values) => {
+    const Data = {
+      service_type: values?.service_type.value,
+      details: values?.details,
+    };
 
-  //     try {
-  //       const response = await createTonar(Data);
-  //       if (response.success) {
-  //         showSuccessMessage(response.message);
-  //       }
-  //     } catch (error) {
-  //       showErrorMessage(error?.response?.data?.message);
-  //     }
-  //   };
+    try {
+      const response = await createResearchServices(Data);
+      if (response.success) {
+        showSuccessMessage(response.message);
+        formik.resetForm();
+      }
+    } catch (error) {
+      showErrorMessage(error?.response?.data?.message);
+    }
+  };
 
-  //Update Toner Installations
-  //   const UpdateTonerAPi = async (values) => {
-  //     const data = {
-  //       requestDate: values?.requestDate,
-  //       fkUserRequestId: values?.fkUserRequestId?.value,
-  //       fkBranchRequestId: values?.fkBranchRequestId,
-  //       fkTonerModelId: values?.tonerModels?.value,
-  //       quantity: values?.quantity,
-  //       status: values?.status,
-  //     };
-  //     try {
-  //       const response = await UpdateTonner(location.state.id, data);
-  //       if (response.success) {
-  //         showSuccessMessage(response.message);
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+  // Update Research Services
+  const UpdateResearchServicesApi = async (values) => {
+    const Data = {
+      service_type: values?.service_type.value,
+      details: values?.details,
+    };
+    try {
+      const response = await UpdateResearchServices(location.state.id, Data);
+      if (response.success) {
+        showSuccessMessage(response.message);
+        formik.resetForm();
+      }
+    } catch (error) {
+      showErrorMessage(error?.response?.data?.message);
+    }
+  };
+
+  const getReserchServicesByIdApi = async () => {
+    try {
+      const response = await getResearchServicesById(location.state.id);
+      if (response.success) {
+        setresearchData(response?.data);
+        // showSuccessMessage(response.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (location.state?.id) {
+      getReserchServicesByIdApi();
+    }
+  }, []);
+
+  useEffect(() => {
+    // Update form values when termsById changes
+    if (researchData) {
+      formik.setValues({
+        service_type:
+          {
+            value: researchData[0]?.service_type,
+            label: researchData[0]?.service_type,
+          } || "",
+        details: researchData[0]?.details || "",
+      });
+    }
+  }, [researchData, formik.setValues]);
 
   return (
     <Layout
@@ -102,7 +174,7 @@ function CMSAddEditResearchService() {
       <div className="container-fluid">
         <div className="card">
           <div className="card-header red-bg" style={{ background: "#666" }}>
-            {location && location?.state ? (
+            {location && location?.state?.id ? (
               <h1>Edit Research Service </h1>
             ) : (
               <h1>Add Research Service </h1>
@@ -112,213 +184,67 @@ function CMSAddEditResearchService() {
             <form onSubmit={formik.handleSubmit}>
               <div className="container-fluid">
                 <div className="row">
-                  <div class="col-4">
+                  <div class="col-6">
                     <div class="mb-3">
-                      <label class="form-label">User Request</label>
+                      <label class="form-label">Service Type</label>
                       <Select
                         options={
-                          employeeData &&
-                          employeeData?.map((item) => ({
-                            value: item.fkUserId,
-                            label: `${item.firstName}${item.lastName}`,
+                          serviceTypeData &&
+                          serviceTypeData?.map((item) => ({
+                            value: item.description,
+                            label: item?.description,
                           }))
                         }
                         onChange={(selectedOptions) => {
-                          formik.setFieldValue(
-                            "fkUserRequestId",
-                            selectedOptions
-                          );
+                          formik.setFieldValue("service_type", selectedOptions);
                         }}
                         onBlur={formik.handleBlur}
-                        value={formik.values.fkUserRequestId}
-                        name="fkUserRequestId"
+                        value={formik.values.service_type}
+                        name="service_type"
                         isClearable={true}
                         className={`.form-select  ${
-                          formik.touched.fkUserRequestId &&
-                          formik.errors.fkUserRequestId
+                          formik.touched.service_type &&
+                          formik.errors.service_type
                             ? "is-invalid"
                             : ""
                         }`}
                       />
-                      {formik.touched.fkUserRequestId &&
-                        formik.errors.fkUserRequestId && (
+                      {formik.touched.service_type &&
+                        formik.errors.service_type && (
                           <div className="invalid-feedback">
-                            {formik.errors.fkUserRequestId}
-                          </div>
-                        )}
-                    </div>
-                  </div>
-                  <div className="col-4">
-                    <div className="mb-3" style={{ position: "relative" }}>
-                      <label className="form-label">From Date</label>
-                      <span
-                        style={{
-                          position: "absolute",
-                          right: "15px",
-                          top: "36px",
-                          zIndex: 1,
-                          fontSize: "20px",
-                          zIndex: "1",
-                          color: "#666",
-                        }}
-                        className={`.form-select  ${
-                          formik.touched.requestDate &&
-                          formik.errors.requestDate
-                            ? "is-invalid"
-                            : ""
-                        }`}
-                      >
-                        <FontAwesomeIcon icon={faCalendarAlt} />
-                      </span>
-                      <DatePicker
-                        selected={formik.values.requestDate}
-                        minDate={new Date()}
-                        onChange={(date) =>
-                          formik.setFieldValue("requestDate", date)
-                        }
-                        onBlur={formik.handleBlur}
-                        className={`form-control`}
-                      />
-                      {formik.touched.requestDate &&
-                        formik.errors.requestDate && (
-                          <div className="invalid-feedback">
-                            {formik.errors.requestDate}
-                          </div>
-                        )}
-                    </div>
-                  </div>
-                  <div className="col-4">
-                    <div className="mb-3" style={{ position: "relative" }}>
-                      <label className="form-label">To Date</label>
-                      <span
-                        style={{
-                          position: "absolute",
-                          right: "15px",
-                          top: "36px",
-                          zIndex: 1,
-                          fontSize: "20px",
-                          zIndex: "1",
-                          color: "#666",
-                        }}
-                        className={`.form-select  ${
-                          formik.touched.requestDate &&
-                          formik.errors.requestDate
-                            ? "is-invalid"
-                            : ""
-                        }`}
-                      >
-                        <FontAwesomeIcon icon={faCalendarAlt} />
-                      </span>
-                      <DatePicker
-                        selected={formik.values.requestDate}
-                        minDate={new Date()}
-                        onChange={(date) =>
-                          formik.setFieldValue("requestDate", date)
-                        }
-                        onBlur={formik.handleBlur}
-                        className={`form-control`}
-                      />
-                      {formik.touched.requestDate &&
-                        formik.errors.requestDate && (
-                          <div className="invalid-feedback">
-                            {formik.errors.requestDate}
+                            {formik.errors.service_type}
                           </div>
                         )}
                     </div>
                   </div>
                 </div>
                 <div className="row">
-                  <div className="col">
-                    <div>
-                      <label className="form-label">Justification</label>
-                      <textarea
-                        className={`form-control  ${
-                          formik.touched.tonardescription &&
-                          formik.errors.tonardescription
-                            ? "is-invalid"
-                            : ""
-                        }`}
-                        id="tonardescription"
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.tonardescription}
-                      ></textarea>
-                      {formik.touched.tonardescription &&
-                        formik.errors.tonardescription && (
-                          <div className="invalid-feedback">
-                            {formik.errors.tonardescription}
-                          </div>
-                        )}
-                    </div>
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="col-4">
-                    <div className="mb-3 mt-4">
-                      <label className="form-label">Select type</label>
-                      <select
-                        class={`form-select ${
-                          formik.touched.complaintStatus &&
-                          formik.errors.complaintStatus
-                            ? "is-invalid"
-                            : ""
-                        }`}
-                        id="complaintStatus"
-                        name="complaintStatus"
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        value={formik.values.complaintStatus}
-                      >
-                        <option value={""} selected disabled hidden>
-                          Select
-                        </option>
-                        <option value="CD">CD</option>
-                        <option value="DVD">DVD</option>
-                      </select>
-                      {formik.touched.complaintStatus &&
-                        formik.errors.complaintStatus && (
-                          <div className="invalid-feedback">
-                            {formik.errors.complaintStatus}
-                          </div>
-                        )}
-                    </div>
-                  </div>
-                  <div class="col-6">
-                    <div class="mb-3">
-                      <div class="form-check">
-                        <input
-                          class={`form-check-input ${
-                            formik.touched.committeeWhole &&
-                            formik.errors.committeeWhole
-                              ? "is-invalid"
-                              : ""
-                          }`}
-                          type="checkbox"
-                          id="committeeWhole"
-                          checked={formik.values.committeeWhole}
-                          onChange={() =>
-                            formik.setFieldValue(
-                              "committeeWhole",
-                              !formik.values.committeeWhole
-                            )
-                          }
-                        />
-                        <label class="form-check-label" for="flexCheckDefault">
-                          Whatsapp
-                        </label>
-                        {formik.touched.committeeWhole &&
-                          formik.errors.committeeWhole && (
-                            <div className="invalid-feedback">
-                              {formik.errors.committeeWhole}
-                            </div>
-                          )}
+                  <div className="col-6">
+                    <label className="form-label">Details</label>
+                    <textarea
+                      className={`form-control  ${
+                        formik.touched.details && formik.errors.details
+                          ? "is-invalid"
+                          : ""
+                      }`}
+                      id="details"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.details}
+                    ></textarea>
+                    {formik.touched.details && formik.errors.details && (
+                      <div className="invalid-feedback">
+                        {formik.errors.details}
                       </div>
-                    </div>
+                    )}
                   </div>
                 </div>
+
                 <div className="d-grid gap-2 mt-4 d-md-flex justify-content-md-end">
                   <button className="btn btn-primary" type="submit">
-                    Submit
+                    {location?.state?.id
+                      ? "Update Services"
+                      : "Create Services"}
                   </button>
                 </div>
               </div>
--- src/pages/Dashboard/Modules/Notice/ResearchServices/index.js
+++ src/pages/Dashboard/Modules/Notice/ResearchServices/index.js
@@ -2,30 +2,67 @@ import { ToastContainer } from "react-toastify";
 import { Layout } from "../../../../../components/Layout";
 import { NoticeSidebarItems } from "../../../../../utils/sideBarItems";
 import Header from "../../../../../components/Header";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import CustomTable from "../../../../../components/CustomComponents/CustomTable";
 import { useNavigate } from "react-router-dom";
+import {
+  showErrorMessage,
+  showSuccessMessage,
+} from "../../../../../utils/ToastAlert";
+import {
+  DeleteResearchServices,
+  getAllResarchServices,
+} from "../../../../../api/APIs/Services/Notice.service";
 
 function CMSResearchServicesDashboard() {
   const navigate = useNavigate();
   const [count, setCount] = useState(null);
   const [currentPage, setCurrentPage] = useState(0);
-  const [researchService, setResearchService] = useState([]);
+  const [researchService, setResearchData] = useState([]);
   const pageSize = 4;
 
   const handlePageChange = (page) => {
     setCurrentPage(page);
   };
-  const transformResearchServiceData = (apiData) => {
+  const transformresarchServicesData = (apiData) => {
     return apiData.map((item, index) => ({
       SR: item?.id,
-      sessionno: `${item?.requestUser?.employee?.firstName}${item?.requestUser?.employee?.lastName}`,
-      fromdate: `${item?.requestBranch?.complaintTypeName}`,
-      todate: `${item?.tonerModel?.tonerModel}`,
-      justification: item?.quantity,
-      status: item?.status,
+      serviceType: item?.service_type,
+      details: item?.details,
+      status: item?.isActive,
     }));
   };
+
+  const getAllResarchServicesApi = useCallback(async () => {
+    try {
+      const response = await getAllResarchServices(currentPage, pageSize);
+      if (response?.success) {
+        setCount(response?.data?.count);
+        const trensferData = transformresarchServicesData(
+          response?.data?.researchServiceData
+        );
+        setResearchData(trensferData);
+      }
+    } catch (error) {
+      showErrorMessage(error?.response?.data?.message);
+    }
+  }, [currentPage, pageSize, setCount, setResearchData]);
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await DeleteResearchServices(id);
+      if (response?.success) {
+        showSuccessMessage(response.message);
+        getAllResarchServicesApi();
+      }
+    } catch (error) {
+      showErrorMessage(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    getAllResarchServicesApi();
+  }, [currentPage]);
   return (
     <Layout
       module={true}
@@ -52,8 +89,12 @@ function CMSResearchServicesDashboard() {
             headertitletextColor={"#FFF"}
             totalCount={count}
             handleAdd={() => navigate("/notice/research-services/addedit")}
-            // handleEdit={(item) => hendleEdit(item.SR)}
-            // handleDelete={(item) => handleDelete(item.SR)}
+            handleEdit={(item) =>
+              navigate("/notice/research-services/addedit", {
+                state: { id: item?.SR },
+              })
+            }
+            handleDelete={(item) => handleDelete(item.SR)}
           />
         </div>
       </div>
