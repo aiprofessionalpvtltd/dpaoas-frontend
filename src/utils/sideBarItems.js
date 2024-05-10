e7bdeb435b8b15591254b2d3519fce9685205a2c
MuhammadSaqibKhan <muhammadsaqibkhan01@gmail.com>
2024-05-10 13:36:41 +0500

    file rivew invoice bill

--- src/utils/sideBarItems.js
+++ src/utils/sideBarItems.js
@@ -717,12 +717,12 @@ export const CMSsidebarItems = [
   },
 
   {
-    itemName: "Admin Complaint",
+    itemName: "Admin",
     link: "/cms/admin/dashboard",
     icon: faHome,
     subItems: [
       {
-        itemName: "Admin Complaint",
+        itemName: "Admin Dashboard",
         link: "/cms/admin/dashboard",
         icon: faClipboardList,
       },
