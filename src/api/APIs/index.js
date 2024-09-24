2c34c5181abec8fb9babf478d289b669a9160cdd
Naseem Ur Rahman <n.rahmanbcs19@gmail.com>
2024-09-24 15:25:32 +0500

    Add Page Size to Correspondence and Remove Inactive From Correspondence

--- src/api/APIs/index.js
+++ src/api/APIs/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import axios from "axios";
 
-const API_URL = "http://10.10.140.89:5152";
+// const API_URL = "http://10.10.140.89:5152";
+const API_URL = process.env.REACT_APP_API_URL;
 const TINY_EDITOR_API_KEY = process.env.REACT_APP_TINY_EDITOR_API_KEY;
 
 console.log("API URL:", API_URL); // This should log the API URL from .env
 console.log("Tiny Editor API Key:", TINY_EDITOR_API_KEY); // This should log the API key from .env
 
 export const imagesUrl = API_URL;
-
 
 const createAxiosClient = (baseURL) => {
   return axios.create({
@@ -20,4 +20,3 @@ const createAxiosClient = (baseURL) => {
 };
 
 export const axiosClient = createAxiosClient(`${API_URL}/api`);
-
